522313e2583e2c3ed6089fc016e2eb2384e11a8f
spestana <spestana@uw.edu>
2019-04-15 11:47:52 -0700

    Improve filename reading with regex

--- datelist.sh
+++ datelist.sh
@@ -7,14 +7,25 @@ if [ -z "$1" ]
         SEARCH_DIR=$1
 fi
 
-echo "Date/Time of ECOSTRESS observations in $SEARCH_DIR" >> ecostress_datelist.csv
-echo "file, day, month, year, time" >> ecostress_datelist.csv
-for f in "$SEARCH_DIR"/*.tif; do
-    YEAR=$(echo $f | cut -d'_' -f6 | cut -c1-4)
-    MONTH=$(echo $f | cut -d'_' -f6 | cut -c5-6) 
-    DAY=$(echo $f | cut -d'_' -f6 | cut -c7-8) 
-    HR=$(echo $f| cut -d'_' -f6 | cut -c10-11)
-    MIN=$(echo $f | cut -d'_' -f6 | cut -c12-13)
-    SEC=$(echo $f | cut -d'_' -f6 | cut -c14-15)
-    echo "${f}, ${DAY}, ${MONTH}, ${YEAR},  ${HR}:${MIN}:${SEC}" >> ecostress_datelist.csv
+if [ -z "$2" ]
+    then
+        out_file="ecostress_datelist.csv"
+    else
+        out_file=$2
+fi
+
+echo "Date/Time of ECOSTRESS observations in $SEARCH_DIR" >> $out_file
+echo "file, day, month, year, time" >> $out_file
+for f in "$SEARCH_DIR"*.tif; do
+    filename=$(echo $f | rev | cut -d'/' -f 1 | rev)
+    datetime_regex="_([0-9]{8}T[0-9]{6})_"
+    if [[ $f =~ $datetime_regex ]]; then
+        datetime=${BASH_REMATCH[1]}; fi
+    YEAR=$(echo $datetime | cut -c1-4)
+    MONTH=$(echo $datetime | cut -c5-6) 
+    DAY=$(echo $datetime | cut -c7-8) 
+    HR=$(echo $datetime | cut -c10-11)
+    MIN=$(echo $datetime | cut -c12-13)
+    SEC=$(echo $datetime | cut -c14-15)
+    echo "${filename}, ${DAY}, ${MONTH}, ${YEAR},  ${HR}:${MIN}:${SEC}" >> $out_file
 done
